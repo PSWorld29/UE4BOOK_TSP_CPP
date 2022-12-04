a4fa6dbd3f74e32a8d07fdc40d86e11f22a1a486
plh2030 <plh2030@naver.com>
2022-12-04 14:29:10 +0900

    소켓추가 gun mesh comp, sniper gun 위치 조정

--- Source/TPSProject/private/TPSPlayer.cpp
+++ Source/TPSProject/private/TPSPlayer.cpp
@@ -42,21 +42,23 @@ ATPSPlayer::ATPSPlayer()
 	JumpMaxCount = 2;
 
 	gunMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("GunMeshComp"));
-	gunMeshComp->SetupAttachment(GetMesh());
+	gunMeshComp->SetupAttachment(GetMesh(), TEXT("hand_rSocket"));
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempGunMesh(TEXT("SkeletalMesh'/Game/FPWeapon/Mesh/SK_FPGun.SK_FPGun'"));
 	if (TempGunMesh.Succeeded())
 	{
 		gunMeshComp->SetSkeletalMesh(TempGunMesh.Object);
-		gunMeshComp->SetRelativeLocation(FVector(-14, 52, 120));
+		gunMeshComp->SetRelativeLocation(FVector(-17, 10, -3));
+		gunMeshComp->SetRelativeRotation(FRotator(0, 90, 0));
 	}
 
 	sniperGumComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SniperGumComp"));
-	sniperGumComp->SetupAttachment(GetMesh());
+	sniperGumComp->SetupAttachment(GetMesh(), TEXT("hand_rSocket"));
 	ConstructorHelpers::FObjectFinder<UStaticMesh> TempSniperMesh(TEXT("StaticMesh'/Game/SniperGun/sniper1.sniper1'"));
 	if (TempGunMesh.Succeeded())
 	{
 		sniperGumComp->SetStaticMesh(TempSniperMesh.Object);
-		sniperGumComp->SetRelativeLocation(FVector(-22, 55, 120));
+		sniperGumComp->SetRelativeLocation(FVector(-42, 7, 1));
+		sniperGumComp->SetRelativeRotation(FRotator(0, 90, 0));
 		sniperGumComp->SetRelativeScale3D(FVector(0.15f));
 	}
 
